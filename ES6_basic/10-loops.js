5c308d282b7d608d12e300571d994a9c98b0e7da
Sabillva <itifstudentsabina@gmail.com>
2024-11-16 16:46:46 +0400

    files

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+    const appendedArray = []
+    for (const string of array) {
+      appendedArray.push(`${appendString}${string}`);
+    }
+  
+    return appendedArray;
+  }
