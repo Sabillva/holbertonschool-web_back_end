9a20331ffd798304bc7d4afe292f22d1a205b675
Sabillva <itifstudentsabina@gmail.com>
2024-12-08 13:50:59 +0400

    files added

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
-"""Await function"""
-import random
+"""Defines an asynchronous coroutine"""
 import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """
-    --------------------
-    METHOD:: wait_random
-    --------------------
-    Description:
-        Sets a temporary halt for a random duration
-        between 0s and value set in @max_delay
-    """
-
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+    """Waits for a random delay between 0 and max_delay"""
+    random_number = random.uniform(0, max_delay)
+    await asyncio.sleep(random_number)
+    return random_number
