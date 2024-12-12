1cfa4a203d6f6b94b1bc9968d1c04a718580ff52
Sabillva <itifstudentsabina@gmail.com>
2024-12-12 20:00:06 +0400

    files added

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
-""" Comprehension project """
-import asyncio
-import time
+""" asyncio.gather """
 
+import asyncio
+import random
+import timeit
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """ Asynchronous coroutine that returns the execution time """
-    start = time.time()
-    tasks = [async_comprehension() for _ in range(4)]
-    """ tasks.append(asyncio.create_task(async_comprehension())) """
-    await asyncio.gather(*tasks)
-    return time.time() - start
+    """ executes comprehension func 4 times in parallel, returns runtime """
+
+    start = timeit.default_timer()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+        )
+    stop = timeit.default_timer()
+    return stop - start
