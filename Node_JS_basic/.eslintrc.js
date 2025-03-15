61aa1e2a9deec488b67a3b79cab31e271f07754d
Sabillva <itifstudentsabina@gmail.com>
2025-03-15 22:47:38 +0400

    add files

--- Node_JS_basic/.eslintrc.js
+++ Node_JS_basic/.eslintrc.js
@@ -1,11 +1,30 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
-  ],
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "plugin:jest/all"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  rules: {},
+  plugins: ["jest"],
+  rules: {
+    "max-classes-per-file": "off",
+    "no-underscore-dangle": "off",
+    "no-console": "off",
+    "no-shadow": "off",
+    "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      excludedFiles: "babel.config.js",
+    },
+  ],
 };
